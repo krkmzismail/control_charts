27d43bce5080a355cd5ee8ea9239559a387a7f46
Gilles Pilon <gillespilon13@gmail.com>
2019-08-06 10:55:28 -0500

    Add docstring, tidy output, fix flake8 errors

--- x_mr_control_charts_example.py
+++ x_mr_control_charts_example.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 
-# control_charts/x_mr_control_charts_example.py
+'''
+Script to read a csv file and create XmR control charts.
 
-from pathlib import Path
+The csv file should have two columns: an index with label, data with label.
+
+./x_mr_control_charts_example.py | tee x_mr.txt
+'''
+
+# from pathlib import Path
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,10 +23,13 @@ chart_data = pd.read_csv(csvfile, index_col=index_column).iloc[:, 0:]
 #                          index_col='Sample').iloc[:, 0:]
 x = X(chart_data, subgroup_size)
 
-print('X chart')
-print('Upper control limit', x.ucl, sep=' = ')
+print('\n\nCSV file:     ', csvfile)
+print('Subgroup size:', subgroup_size)
+print('Index column: ', index_column)
+print('\nX chart')
+print('Upper control limit ', x.ucl, sep=' = ')
 print('Average moving range', x.mean, sep=' = ')
-print('Lower control limit', x.lcl, sep=' = ')
+print('Lower control limit ', x.lcl, sep=' = ')
 print(f'Sigma(X)', x.sigma, sep=' = ')
 for i in range(-3, 4):
     print(f'{i} Sigma', ' '.join(map(str, [x.sigmas[i]])), sep=' = ')
@@ -28,14 +37,14 @@ ax1 = x.ax
 ax1.set_title('X control chart' + '\n' 'Subtitle')
 ax1.set_ylabel('Response (units)')
 ax1.set_xlabel('X axis label')
-ax1.figure.savefig('x.svg', format='svg') # Comment if you wish interactive
+ax1.figure.savefig('x.svg', format='svg')  # Comment if you wish interactive
 # plt.show() # Uncomment if you wish interactive
-plt.clf() # Comment if you wish interactive
+plt.clf()  # Comment if you wish interactive
 mr = mR(chart_data, subgroup_size)
-print('mR chart')
-print('Upper control limit', mr.ucl, sep=' = ')
+print('\nmR chart')
+print('Upper control limit ', mr.ucl, sep=' = ')
 print('Average moving range', mr.mean, sep=' = ')
-print('Lower control limit', mr.lcl, sep=' = ')
+print('Lower control limit ', mr.lcl, sep=' = ')
 print(f'Sigma(X)', mr.sigma, sep=' = ')
 for i in range(-3, 4):
     print(f'{i} Sigma', ' '.join(map(str, [mr.sigmas[i]])), sep=' = ')
@@ -43,6 +52,6 @@ ax2 = mr.ax
 ax2.set_title('mR control chart' + '\n' 'Subtitle')
 ax2.set_ylabel('Response (units)')
 ax2.set_xlabel('X axis label')
-ax2.figure.savefig('mr.svg', format='svg') # Comment if you wish interactive
+ax2.figure.savefig('mr.svg', format='svg')  # Comment if you wish interactive
 # plt.show() # Uncomment if you wish interactive
-plt.clf() # Comment if you wish interactive
+plt.clf()  # Comment if you wish interactive
